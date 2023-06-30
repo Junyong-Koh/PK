5e0c9060f1b9f03b7d6168d6b03ceb52614b6f94
Junyong-Koh <78996798+Junyong-Koh@users.noreply.github.com>
2023-06-30 10:54:26 +0900

    Update main_pk.py

--- code/main_pk.py
+++ code/main_pk.py
@@ -11,6 +11,29 @@ def set_random_seed(seed = 0):
   torch.manual_seed(seed)
   if torch.cuda.is_available():
       torch.cuda.manual_seed(seed)
+
+def make_dataloader(input_data, batch_size):
+  ### Total Data
+  NumOfData = len(input_data)
+  ### Train Data
+  NumOfTrain = int(NumOfData * 0.8)
+  ### Validation Data
+  NumOfVal = int(NumOfData * 0.1)
+  ### Test Data
+  NumOfTest = NumOfData - NumOfTrain - NumOfVal
+  
+  ### SHOW
+  print("Num of Train: ", NumOfTrain)
+  print("Num of val: ", NumOfVal)
+  print("Num of Test: ", NumOfTest)
+  
+  ### Split Data
+  Trainset, Valset, Testset = random_split(input_data, [NumOfTrain, NumOfVal, NumOfTest])
+  ### Make Dataloader
+  train_dataloader = DataLoader(Trainset, batch_size = batch_size, shuffle = True, drop_last = False)
+  val_dataloader = DataLoader(Valset, batch_size = batch_size, shuffle = True, drop_last = False)
+  test_dataloader = DataLoader(Testset, batch_size = batch_size, shuffle = True, drop_last = False)
+  return train_dataloader, val_dataloader, test_dataloader
       
 ### Data parameter
 SEED = 123
@@ -36,3 +59,13 @@ set_random_seed(SEED)
 
 ### Load data
 total_input_data, smiles, label, drug, patient_info, route_info, norm_time_info, norm_pk_info = make_input()
+
+### CASE1: Use processed dataset
+train_dataloader = torch.load("TRAIN_DATA.pth")
+val_dataloader = torch.load("VAL_DATA.pth")
+test_dataloader = torch.load("TEST_DATA.pth")
+
+'''
+### CASE2: Use random dataset
+train_dataloader, val_dataloader, test_dataloader = make_dataloader(total_input_data, BATCH_SIZE)
+'''
